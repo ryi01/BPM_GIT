6f9aaea03f08c0290f688848a2a35108e0cd0be7
ryi01 <55128336+ryi01@users.noreply.github.com>
2022-07-28 16:31:49 +0900

    Initial commit

---
+++ Assets/MK_Scripts/Larva.cs
@@ -0,0 +1,65 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// 애벌레 : 느리게 계속 점프하면서 플레이어를 따라다님
+public class Larva : MonoBehaviour
+{
+    // 애벌레 속도
+    public float speed = 3.0f;
+    // 점프 힘
+    public float jumpPow = 10;
+    // 점프 하는 시간
+    public float jumpTime = 1;
+    // 플레이어와의 거리
+    public float moveDis = 2;
+    // 넉백 힘
+    public float backPow = 3;
+
+    // 플레이어 위치
+    Transform player;
+    // 리지드바디
+    Rigidbody rigid;
+    // 방향
+    Vector3 dir;
+    // Start is called before the first frame update
+    void Start()
+    {
+        // 플레이어 찾기
+        player = GameObject.Find("Dummy_Player").transform;
+        // 리지드바디 가져오기
+        rigid = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.LookAt(player);
+
+        // 방향 설정
+        dir = player.position - transform.position;
+        dir.Normalize();
+
+        // 플레이어 향하기
+        transform.position += dir * speed * Time.deltaTime;
+    }
+
+    // 넉백용 함수
+    public void NockBack()
+    {
+        rigid.AddForce(-dir * backPow, ForceMode.Impulse);
+    }
+
+    // 땅에 있을때 점프
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.name.Contains("Floor"))
+        {
+            rigid.AddForce(Vector3.up * jumpPow, ForceMode.Impulse);
+        }
+        if (collision.gameObject.name.Contains("Player"))
+        {
+            NockBack();
+        }
+    }
+}
---
+++ Assets/MK_Scripts/Spider.cs
@@ -0,0 +1,143 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// 거미 : 랜덤 시간마다 플레이어의 위치로 점프하거나 돌진함
+// FSM 사용해서 Move, Jump, Run으로 바꿔야할듯
+public class Spider : MonoBehaviour
+{
+    // 필요속성 : 현재 시간
+    float currentTime = 0;
+    // 속도
+    float speed = 4;
+    // 플레이어
+    Transform player;
+    // 방향
+    Vector3 dir;
+    // 멈추는 시간
+    float stopTime = 3;
+
+    enum SpiderState
+    {
+        Move,
+        Run,
+        Jump,
+        Stop
+    }
+    SpiderState state;
+    // Start is called before the first frame update
+    void Start()
+    {
+        // 플레이어 찾기
+        player = GameObject.Find("Dummy_Player").transform;
+        state = SpiderState.Move;
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.LookAt(player);
+        dir = player.transform.position - transform.position;
+
+        if (state == SpiderState.Move)
+        {
+            SpidexrMove();
+        }
+        else if (state == SpiderState.Stop)
+        {
+            SpiderStop();
+        }
+        else if(state == SpiderState.Run)
+        {
+            SpiderRun();
+        }
+        else if (state == SpiderState.Jump)
+        {
+            SpiderJump();
+        }
+
+    }
+
+    // 기본적인 움직임
+    // 플레이어쪽으로 향하다가 랜덤 시간에 Stop으로 바뀐다
+    private void SpidexrMove()
+    {
+        // 움직임
+        transform.position += dir * speed * Time.deltaTime;
+
+        // 랜덤 시간이 되면
+        currentTime += Time.deltaTime;
+        int rndTime = Random.Range(2, 5);
+        if (currentTime > rndTime)
+        {
+            // stop으로 바뀌기
+            state = SpiderState.Stop;
+            currentTime = 0;
+        }
+
+    }
+
+    // 그 자리에 서서 멈추고 플레이어만 바라보다가 일정 시간이 지난 후, 런이나 점프 모드로 바뀜
+    private void SpiderStop()
+    {
+        Debug.Log("Stop");
+        // 그 자리에 멈추지만, 플레이어는 바라봐야함.
+        transform.position += dir * 0 * Time.deltaTime;
+        // 일정시간이 지나면
+        currentTime += Time.deltaTime;
+        if(currentTime > stopTime)
+        {
+            int rndAttack = Random.Range(0, 1);
+            if(rndAttack == 0)
+            {
+                state = SpiderState.Run;
+            }
+            else
+            {
+                state = SpiderState.Jump;
+            }
+        }
+        
+    }
+    private void SpiderRun()
+    {
+        Debug.Log("RUN");
+    }
+
+    private void SpiderJump()
+    {
+        Debug.Log("Jump");
+    }
+
+ /*   void SpiderAttack(Vector3 dir)
+    {        
+        // 랜덤으로 할 행동을 고르고
+        int rnd = Random.Range(0, 1);
+        if (rnd == 0)
+        {
+            // 랜덤시간마다
+            float rndTime = Random.Range(3, 10);
+            // 시간이 흐르고
+            currentTime += Time.deltaTime;
+            if (rndTime < currentTime)
+            {
+                // 그쪽으로 점프하고 싶다
+                transform.position = Vector3.Slerp(transform.position, player.transform.position, 0.05f);
+            }
+        }
+        else
+        {
+            // 랜덤시간마다
+            float rndTime = Random.Range(3, 10);
+            // 시간이 흐르고
+            currentTime += Time.deltaTime;
+            if (rndTime < currentTime)
+            {
+                // 그쪽으로 점프하고 싶다
+                transform.position += dir * speed * 2 * Time.deltaTime;
+            }
+        }
+
+    }*/
+}
