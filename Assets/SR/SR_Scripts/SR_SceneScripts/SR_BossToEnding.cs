77e4210f33c2894804bb9b40dff52bb4560a917b
Love-Kang <110151460+Love-Kang@users.noreply.github.com>
2022-08-03 11:03:59 +0900

    Load Scene Setting(Except Enemy2Scene)

---
+++ Assets/SR/SR_Scripts/SR_SceneScripts/SR_BossToEnding.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SR_BossToEnding : MonoBehaviour
+{
+    
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.name.Contains("Player"))
+        {
+            SceneManager.LoadScene(7);
+        }
+    }
+}
+
