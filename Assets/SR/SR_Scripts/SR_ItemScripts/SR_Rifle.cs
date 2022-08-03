a6ba0b877d3d9b3a68f23b78b4e3907c5fa9c65b
Love-Kang <110151460+Love-Kang@users.noreply.github.com>
2022-08-03 09:41:12 +0900

    Shoot Enemy Error Fix (Gun Scripts)

--- Assets/SR/SR_Scripts/SR_ItemScripts/SR_Rifle.cs
+++ Assets/SR/SR_Scripts/SR_ItemScripts/SR_Rifle.cs
@@ -119,26 +119,23 @@ public class SR_Rifle : MonoBehaviour
         {
             Debug.Log(hit.transform.name);
 
-            GameObject larva = GameObject.Find("Larva");
             if (hit.transform.name.Contains("Larva"))
             {
-                larva.GetComponent<LarvaHP>().AddDamage(damage);
-                larva.GetComponent<Larva>().NockBack();
+                hit.transform.GetComponent<LarvaHP>().AddDamage(damage);
+                hit.transform.GetComponent<Larva>().NockBack();
             }
 
-            GameObject bat = GameObject.Find("Bat");
-            if (hit.transform.name.Contains("Bat")) bat.GetComponent<BatHP>().AddDamage(damage);
+            if (hit.transform.name.Contains("Bat")) hit.transform.GetComponent<BatHP>().AddDamage(damage);
 
-            GameObject spider = GameObject.Find("Spider");
             if (hit.transform.name.Contains("Spider"))
             {
-                spider.GetComponent<SpiderHP>().AddDamage(damage);
-                spider.GetComponent<Spider>().NockBack();
+                hit.transform.GetComponent<SpiderHP>().AddDamage(damage);
+                hit.transform.GetComponent<Spider>().NockBack();
 
             }
 
-            GameObject boss = GameObject.Find("Boss");
-            if (hit.transform.name.Contains("Boss")) boss.GetComponent<BossHP>().AddDamage(damage);
+
+            if (hit.transform.name.Contains("Boss")) hit.transform.GetComponent<BossHP>().AddDamage(damage);
 
             Instantiate(impactEffect, hit.point, Quaternion.LookRotation(hit.normal));
         }
