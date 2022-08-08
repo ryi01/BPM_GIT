be001cb1c30148e1ab95f3336dba52fce2b7be12
Love-Kang <110151460+Love-Kang@users.noreply.github.com>
2022-08-08 15:13:47 +0900

    Add Scene Manager

--- Assets/SR/SR_Scripts/SR_SceneScripts/SR_Enemy2ToLibrary.cs
+++ Assets/SR/SR_Scripts/SR_SceneScripts/SR_Enemy2ToLibrary.cs
@@ -3,14 +3,14 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-public class SR_Enemy1ToEnemy2 : MonoBehaviour
+public class SR_Enemy2ToLibrary : MonoBehaviour
 {
     
     private void OnTriggerEnter(Collider other)
     {
         if(other.name.Contains("Player"))
         {
-            SceneManager.LoadScene(4);
+            SceneManager.LoadScene(2);
 
         }
     }
