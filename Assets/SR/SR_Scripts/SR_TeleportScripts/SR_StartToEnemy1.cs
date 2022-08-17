20bbce619cb52490710b6b01135b599c93496b58
Love-Kang <110151460+Love-Kang@users.noreply.github.com>
2022-08-17 14:51:44 +0900

    Teleport Complete

---
+++ Assets/SR/SR_Scripts/SR_TeleportScripts/SR_StartToEnemy1.cs
@@ -0,0 +1,57 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SR_StartToEnemy1 : MonoBehaviour
+{
+    public Image black;
+    Color color;
+    Transform player;
+    public Transform newPos;
+
+    private void Start()
+    {
+        color = black.GetComponent<Image>().color;
+        color.a = 0;
+        black.GetComponent<Image>().color = color;
+    }
+
+
+    IEnumerator FadeIn()
+    {
+        float fadeCount = 0;
+        while(fadeCount<1.0f)
+        {
+            fadeCount += 0.01f;
+            yield return new WaitForSeconds(0.01f);
+            black.color = new Color(0, 0, 0, fadeCount);
+        }
+    }
+    IEnumerator FadeOut()
+    {
+        float fadeCount = 1.0f;
+        while (fadeCount >= 0)
+        {
+            fadeCount -= 0.01f;
+            yield return new WaitForSeconds(0.01f);
+            black.color = new Color(0, 0, 0, fadeCount);
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        //player = GameObject.Find("Player").transform;
+        //StartCoroutine(FadeIn());
+        //StartCoroutine(FadeOut());
+        //player.GetComponent<Transform>().position = newPos.position;
+
+        if(other.name.Contains("Player"))
+        {
+            StartCoroutine(FadeIn());
+            StartCoroutine(FadeOut());
+            other.GetComponent<Transform>().position = newPos.position;
+        }
+    }
+    
+}
